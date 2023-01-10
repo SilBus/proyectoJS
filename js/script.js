d1f5f8dba6d79dbb202d32cf32ca182760849172
silvina <silvina.busi@gmail.com>
2023-01-10 13:49:51 -0300

    modificacion del for

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 let nombre = prompt("Hola! Ingresa tu nombre, por favor")
 let apellido = prompt("Ahora ingresa tu apellido")
-const IVA = 1.21
-
 
 if ((nombre != "") && (apellido != "")) {//
     alert("Bienvenido" + " " + nombre + " " + apellido)
@@ -19,24 +17,51 @@ do{
     }
 } while (contrasena == "")
 
-for(let producto=1; producto <= 5; producto++){
-    let productoSelecionado = prompt("Seleccione 5 (cinco) productos del stock")
+
+let cantidad_producto = Number(prompt("ingrese cantidad de productos seleccionados (hasta 7)"))
+let total = 0
+const IVA = 1.21
+
+for (let index = 1; index <= cantidad_producto; index++) {
+    let nombre = prompt("ingrese nombre del producto")
+    let monto = Number(prompt("ingrese el valor del producto seleccionado"))
+total = monto * IVA
+console.log(`producto ${index} con nombre ${nombre} y monto ${monto} El total de todo es ${total}`)
     
-    if(productoSelecionado == ""){
-        break;
-    }else(
-        alert("Seleccionaste" + " " + producto + " " + "productos")
-    )
 }
 
 
-function multiplicar(monto , IVA) {
-    let total = monto * IVA
-    return total
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for(let producto=1; producto <= 5; producto++){
+//    let productoSelecionado = prompt("Seleccione 5 (cinco) productos del stock")
     
-}
-let resultado = multiplicar(5,1.21)
-console.log(`el valor total IVA incluido es de ${resultado} pesos`)
+//    if(productoSelecionado == ""){
+//        break;
+//    }else(
+//        alert("Seleccionaste" + " " + producto + " " + "productos")
+//    )
+//}
+
+
+//function multiplicar(monto , IVA) {
+//    let total = monto * IVA
+//    return total
+    
+//}
+//let resultado = multiplicar(5,1.21)
+//console.log(`el valor total IVA incluido es de ${resultado} pesos`)
 
 
 
